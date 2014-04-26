6d8790be8829687b4a7c4c33802ee3c4bc0929b2
dglgmut <florez.renan@gmail.com>
2014-04-25 22:30:11 -0300

    Creating asana dashboard

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 asana_dashboard
-===============
---
+++ test/asana_dashboard/web_test.clj
@@ -0,0 +1,6 @@
+(ns asana_dashboard.web-test
+  (:require [clojure.test :refer :all]
+            [asana_dashboard.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))

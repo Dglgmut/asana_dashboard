5d28e7ba132ebba1b8577f874aa97c0934a38acb
dglgmut <florez.renan@gmail.com>
2014-04-25 23:10:47 -0300

    adding ring as a plugin and dependency

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "http://example.com/FIXME"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring/ring-devel "1.1.0"]
+                 [ring "1.2.2"]
                  [ring-basic-authentication "1.0.1"]
                  [environ "0.2.1"]
                  [com.cemerick/drawbridge "0.0.6"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[environ/environ.lein "0.2.1"]
+            [lein-ring "0.8.10"]]
   :hooks [environ.leiningen.hooks]
-  :profiles {:production {:env {:production true}}})
+  :profiles {:production {:env {:production true}}}
+  :ring {:handler asana_dashboard.web/app})

063c08529cd5ea4513e55256faea18a82a26e76e
dglgmut <florez.renan@gmail.com>
2014-04-26 06:59:04 -0300

    defining basic API get

--- src/asana_dashboard/asana_data_import.clj
+++ src/asana_dashboard/asana_data_import.clj
@@ -1,6 +1,9 @@
 (ns asana_dashboard.asana-data-import
   (:require [clj-http.client :as client]))
 
-(defn get_asana_page
-  [url]
-  (client/get url))
+(defn get-asana-page
+  [api-path auth-token]
+  (client/get
+    (str "https://app.asana.com/api/1.0/"
+         api-path)
+    {:basic-auth (str auth-token ":")}))

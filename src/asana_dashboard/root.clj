8d76db05d74a8e6d515156091f10a47f9b31c045
dglgmut <florez.renan@gmail.com>
2014-04-27 12:37:23 -0300

    Removing routes and controllers layer from joodo framwork

--- src/asana_dashboard/root.clj
+++ src/asana_dashboard/root.clj
@@ -1,15 +1,14 @@
 (ns asana-dashboard.root
   (:use
+    [asana-dashboard.lib]
     [compojure.core :only (defroutes GET POST)]
     [compojure.route :only (not-found)]
     [joodo.middleware.view-context :only (wrap-view-context)]
-    [joodo.views :only (render-template render-html)]
-    [joodo.controllers :only (controller-router)]))
+    [joodo.views :only (render-template render-html)]))
 
 (defroutes asana-dashboard-routes
   (GET "/" [] (render-template "index"))
-  (POST "/" {:as req} (str req))
-  (controller-router 'asana-dashboard.controller)
+  (POST "/" {params :params} (str (:asana-token params)))
   (not-found (render-template "not_found" :template-root "asana_dashboard/view" :ns `asana-dashboard.view.view-helpers)))
 
 (def app-handler

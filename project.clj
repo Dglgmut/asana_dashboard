edd6ae181f5d888d7caa3342efaeecde0d0ad9ca
dglgmut <florez.renan@gmail.com>
2014-04-29 23:41:09 -0300

    Adding clojurescript dependency

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A simple stand-alone webapp"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
+                 [org.clojure/clojurescript "0.0-2202"]
                  [clj-http "0.9.1"]
                  [joodo "1.1.2"]]
   :min-lein-version "2.0.0"
@@ -12,6 +13,7 @@
   :test-paths ["spec/"]
   :java-source-paths ["src/"]
   :plugins [[speclj "2.6.1"]
-            [joodo/lein-joodo "1.1.2"]]
+            [joodo/lein-joodo "1.1.2"]
+            [lein-cljsbuild "1.0.3"]]
 
   )
